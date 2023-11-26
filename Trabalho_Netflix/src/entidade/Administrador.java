711641d5dfbed3748ce81c6d5b1f09d81d5141bb
Vitor Campos <vlfc.eng@gmail.com>
2023-11-26 14:24:34 -0300

    Alteração da classe Administrador de static para final

--- Trabalho_Netflix/src/entidade/Administrador.java
+++ Trabalho_Netflix/src/entidade/Administrador.java
@@ -1,16 +1,12 @@
 package entidade;
 
 public class Administrador {
-	private static String Senha = "admin";
+	private final String senha = "admin";
 
-	public static String getSenha() {
-		return Senha;
+	public String getSenha() {
+		return senha;
 	}
 
-	public static void setSenha(String senha) {
-		Senha = senha;
-	}
-	
 	public Boolean confereSenha(String senha) {
 		return senha.equals(getSenha());
 	}
